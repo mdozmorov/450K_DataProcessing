a942a2b9c9710be412ce00e214154ad8e10918eb
Mikhail Dozmorov <mikhail.dozmorov@gmail.com>
2016-03-22 13:19:51 -0400

    Add an option to select which chromosomes to remove

--- SRC/filterXY.R
+++ SRC/filterXY.R
@@ -1,17 +1,21 @@
 # Removes probes associated to allosomes (chr X and Y)
 # 
 # Args: methylumi object
+#       chr_remove which chromosomes to remove. Default - X, Y
 #
 # Returns: methylumi object
 
-filterXY <- function(methLumi_data){
-
-	probeXY <- fData(methLumi_data)$TargetID[ which(fData(methLumi_data)$CHR=="Y" | fData(methLumi_data)$CHR=="X") ]
+filterXY <- function(methLumi_data, chr_remove=c("X", "Y")){
+  probeXY <- vector(mode = "character")
+  for (i in chr_remove) {
+    probeXY <- c(probeXY, as.character(fData(methLumi_data)$TargetID[ which(fData(methLumi_data)$CHR == i) ]))
+  }
+	
 	indexXY <- which(is.element(featureNames(methLumi_data), probeXY))
 	rm(probeXY)
-	if(length(indexXY) > 0) methLumi_data <- methLumi_data[-indexXY,]
+	if(length(indexXY) > 0) methLumi_data <- methLumi_data[-indexXY, ]
 	
-	print(paste("Nb probes on X & Y chr to remove: ", length(indexXY), sep=""))
+	print(paste("Number of probes on chromosomes ", paste(chr_remove, collapse = " "), " to remove: ", length(indexXY), sep=""))
 	
 	return(methLumi_data)
 }
